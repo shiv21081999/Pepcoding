f381204de15a007a6ab88a9ea5f7cd9fa001bae1
Shivam Sharma <shivamsharma@Shivams-MacBook-Pro.local>
2020-11-07 16:15:54 +0530

    random

--- New/Main.java
+++ New/Main.java
@@ -4,7 +4,13 @@ import java.util.*;
 public class Main {
 
   public static void combinations(int cb, int tb, int ssf, int ts, String asf){
-    // write your code here
+    if(cb == tb +1)
+    {
+      System.out.println(asf);
+      return ;
+    }
+    combinations(cb+1, tb, ssf+1, ts, asf+"i");
+    combinations(cb+1, tb, ssf, ts, asf+"_");
   }
 
   public static void main(String[] args) throws Exception {
