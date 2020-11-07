1add4572de754e6439f6820ea21d871da997c6b2
Shivam Sharma <shivamsharma@Shivams-MacBook-Pro.local>
2020-11-07 16:07:03 +0530

    permutations 1

--- New/Main.java
+++ New/Main.java
@@ -3,37 +3,15 @@ import java.util.*;
 
 public class Main {
 
-    static String max;
-    
-    public static String swap(String str, int i , int j)
-    {
-        StringBuilder sb = new StringBuilder(str);
-        sb.setCharAt(i, str.charAt(j));
-        sb.setCharAt(j, str.charAt(i));
-        return sb.toString();
-    }
-	public static void findMaximum(String str, int k) {
-        if(k == 0)
-        {
-            if(Integer.parseInt(str) > Integer.parseInt(max))
-                max = str;
-            return;
-        }
-        if(Integer.parseInt(str) > Integer.parseInt(max))
-            max = str;
-        for(int i = 0; i < str.length()-1; i++)
-        {
-            for(int j = i + 1; j < str.length() ; j++)
-                findMaximum(swap(str, j, i), k-1);
-        }
-	}
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.next();
-		int k = scn.nextInt();
-		 max = str;
-		findMaximum(str, k);
-		System.out.println(max);
-	}
+  public static void combinations(int cb, int tb, int ssf, int ts, String asf){
+    // write your code here
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(1, nboxes, 0, ritems, "");
+  }
 
 }
